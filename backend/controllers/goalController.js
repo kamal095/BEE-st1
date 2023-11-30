7acc36f8e60ce209e27df51b1c81d057d2984e0a
kamal095 <102540199+kamal095@users.noreply.github.com>
2023-11-30 18:48:27 +0530

    Add files via upload

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,58 @@
+const asyncHandler = require ('express-async-handler')
+
+const Goal = require ('../model/goalModel')
+
+const { error } = require("console")
+
+const getGoals =asyncHandler( async(req ,res)=> {
+    const goals = await Goal.find()
+    res.status(200).json(goals)
+})
+
+const setGoals = asyncHandler(async(req,res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('please add text ')
+    }
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
+})
+
+const updateGoal = asyncHandler(async(req,res) => {
+    const goal =await Goal.findById(req.params.id)
+    
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not Found')
+    }
+
+    const updatedGoal= await Goal.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
+})
+
+const deleteGoal =asyncHandler(async (req,res) => {
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not Found')
+    }
+    await goal.deleteOne()
+
+    res.status(200).json({id: req.params.id})
+})
+
+module.exports = {
+    getGoals,
+    setGoals,
+    updateGoal,
+    deleteGoal,
+}
